32fb1b79a3dc4fb8af90bd938bafa7184c1fc5dc
André Marques <andre.m.d.abreu@gmail.com>
2023-09-26 11:58:01 -0300

    Inicio do exercício 2

---
+++ src/Tecnico.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "Livro.php";
+    class Tecnico extends Livro {
+        private array $formato = ["digital", "fisico"];
+
+        public function getFormato():string {
+            return implode($this->formato);
+        }
+        public function setFormato(array $formato):void {
+            $this->formato = $formato;
+        }
+    }
