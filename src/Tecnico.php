9e914321ef8133f021525f7993c342615ec2a961
André Marques <andre.m.d.abreu@gmail.com>
2023-10-03 09:20:10 -0300

    Update Tecnico.php

--- src/Tecnico.php
+++ src/Tecnico.php
@@ -1,16 +1,17 @@
 <?php
+    namespace Biblioteca;
     require_once "Livro.php";
     class Tecnico extends Livro {
         private array $formato = ["digital", "fisico"];
 
         public function getFormato(): string
         {
-                return implode(", ", $this->formato);
+            return implode(", ", $this->formato);
         }
         public function setFormato(array $formato): self
         {
-                $this->formato = $formato;
+            $this->formato = $formato;
 
-                return $this;
+            return $this;
         }
     }
