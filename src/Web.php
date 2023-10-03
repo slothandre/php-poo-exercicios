f80bd52327a7592e20a655f4309ef49d4280ceac
André Marques <andre.m.d.abreu@gmail.com>
2023-10-03 09:54:17 -0300

    Update Web.php

--- src/Web.php
+++ src/Web.php
@@ -1,6 +1,5 @@
 <?php
     namespace Biblioteca;
-    require_once "Programacao.php";
     class Web extends Programacao {
         public function __construct() {
             echo "<h2>Livros de Programação Web</h2>";
